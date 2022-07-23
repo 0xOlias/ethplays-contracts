f95f850845d975167c776a726ca04053f579cb93
Olias <0xolias@gmail.com>
2022-07-23 11:13:44 -0700

    wip: registry contract with fee instead of ens

--- src/Registry.sol
+++ src/Registry.sol
@@ -3,34 +3,21 @@ pragma solidity ^0.8.13;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-abstract contract ENS {
-    function resolver(bytes32 node) public view virtual returns (Resolver);
-}
-
-abstract contract Resolver {
-    function addr(bytes32 node) public view virtual returns (address);
-}
-
 /// @title Mainnet registration contract for EthPlays
 /// @author olias.eth
 /// @notice This is experimental software, use at your own risk.
 contract Registry is Ownable {
     /* -------------------------------------------------------------------------- */
-    /*                                   CONFIG                                   */
-    /* -------------------------------------------------------------------------- */
-
-    ENS public constant ens = ENS(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
-
-    /* -------------------------------------------------------------------------- */
     /*                                   STORAGE                                  */
     /* -------------------------------------------------------------------------- */
 
-    /// @notice Indicates if an ENS name is currently required for registration
-    bool public ensRequired;
     /// @notice Registered account addresses by burner account address
     mapping(address => address) public accounts;
     /// @notice Burner account addresses by registered account address
     mapping(address => address) public burnerAccounts;
+
+    /// @notice Registration fee amount in ether
+    uint256 public registrationFeeAmount;
 
     /* -------------------------------------------------------------------------- */
     /*                                   EVENTS                                   */
@@ -46,14 +33,14 @@ contract Registry is Ownable {
     error BurnerAccountTaken();
     error AlreadyRegistered();
     error NotYetRegistered();
-    error InvalidEnsNameHash();
+    error InsufficientRegistrationFee();
 
     /* -------------------------------------------------------------------------- */
     /*                               INITIALIZATION                               */
     /* -------------------------------------------------------------------------- */
 
     constructor() {
-        ensRequired = false;
+        registrationFeeAmount = 0.01 ether;
     }
 
     /* -------------------------------------------------------------------------- */
@@ -61,9 +48,8 @@ contract Registry is Ownable {
     /* -------------------------------------------------------------------------- */
 
     /// @notice Register for EthPlays.
-    /// @param ensNameHash An ENS namehash that must resolve to the sender's address.
     /// @param burnerAccount The address of the burner account to be registered.
-    function register(bytes32 ensNameHash, address burnerAccount) external {
+    function register(address burnerAccount) external payable {
         if (accounts[burnerAccount] != address(0)) {
             revert BurnerAccountTaken();
         }
@@ -72,8 +58,8 @@ contract Registry is Ownable {
             revert AlreadyRegistered();
         }
 
-        if (ensRequired && msg.sender != resolve(ensNameHash)) {
-            revert InvalidEnsNameHash();
+        if (msg.value < registrationFeeAmount) {
+            revert InsufficientRegistrationFee();
         }
 
         accounts[burnerAccount] = msg.sender;
@@ -100,19 +86,23 @@ contract Registry is Ownable {
     }
 
     /* -------------------------------------------------------------------------- */
-    /*                                  HELPERS                                   */
-    /* -------------------------------------------------------------------------- */
-
-    function resolve(bytes32 node) internal view returns (address) {
-        Resolver resolver = ens.resolver(node);
-        return resolver.addr(node);
-    }
-
-    /* -------------------------------------------------------------------------- */
     /*                                   ADMIN                                    */
     /* -------------------------------------------------------------------------- */
 
-    function setEnsRequired(bool _ensRequired) external onlyOwner {
-        ensRequired = _ensRequired;
+    function setRegistrationFeeAmount(uint256 _registrationFeeAmount) external onlyOwner {
+        registrationFeeAmount = _registrationFeeAmount;
     }
+
+    // ???
+    // function withdrawEther(address withdrawToAccount) external onlyOwner {
+    //     _account.call{value: this.value}()
+    // }
+
+    // function withdrawERC20(address withdrawToAccount, address token) external onlyOwner {
+    //     _account.call{value: this.value}()
+    // }
+
+    // function withdrawERC721(address withdrawToAccount, address token) external onlyOwner {
+    //     _account.call{value: this.value}()
+    // }
 }
